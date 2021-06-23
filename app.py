4b26e256532d419352754354a979b23c4646a6f3
romanceformoon <lmwljw98@gmail.com>
2021-06-23 14:39:21 +0900

    Changed HTML, Function defining, Added session

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
-import asyncio
 import aioredis
 import jinja2
 import aiohttp_jinja2
 import aiohttp
-from views import *
+import base64
+from views import routes
+from cryptography import fernet
+from aiohttp_session import setup
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+import os
+import pickle
 
 
 async def make_app():
     app = aiohttp.web.Application()
+    if os.path.exists(".secret"):
+        with open(".secret", "rb") as f:
+            fernet_key = pickle.load(f)
+    else:
+        fernet_key = fernet.Fernet.generate_key()
+        with open(".secret", "wb") as f:
+            pickle.dump(fernet_key, f)
+    secret_key = base64.urlsafe_b64decode(fernet_key)
+    setup(app, EncryptedCookieStorage(secret_key))
     app.add_routes(routes)
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('./templates/'))
 
     # Create a redis
-    app['redis'] = await aioredis.create_redis(('localhost', 6379), db=1, encoding='utf-8')
+    app['redis'] = await aioredis.create_redis(
+        ('localhost', 6379), db=1, encoding='utf-8')
 
     # Initialize redis at first
-    await app['redis'].delete('name') 
+    await app['redis'].delete('name')
     app['online_users'] = {}
 
     return app
--- views.py
+++ views.py
@@ -1,19 +1,44 @@
-import asyncio
 import random
-import jinja2
+import string
 import aiohttp_jinja2
 import aiohttp
+from aiohttp_session import get_session
 
 
 routes = aiohttp.web.RouteTableDef()
 
+
 def generateRandomName():
-    return "Anonymous" + str(random.randint(0, 9999))
+    string_pool = string.ascii_letters + string.digits
+    result = ""
+    for i in range(4):
+        result += random.choice(string_pool)
+    return "Anonymous_" + result
+
+
+async def broadcast(users, **kwargs):
+    for _ws in users.values():
+        try:
+            await _ws.send_json(kwargs)
+        except OSError:
+            # If users have disconnected websocket.
+            continue
+
 
 @routes.get('/')
 async def index(request):
+    # Get session
+    session = await get_session(request)
+    # If have no session, make new.
+    if session.new:
+        nickname = generateRandomName()
+        session['nickname'] = nickname
+    else:
+        nickname = session['nickname']
+
     r = request.app['redis']
     users = request.app['online_users']
+
     ws = aiohttp.web.WebSocketResponse()
     # Check if websocket can be started.
     if not ws.can_prepare(request).ok:
@@ -21,38 +46,49 @@ async def index(request):
     # Start websocket.
     # After the call you can use websocket methods.
     await ws.prepare(request)
-    
-    nickname = generateRandomName()
-    count = int(await r.zcard('name'))
-    await r.zadd('name', count + 1, nickname)
+
+    # Make clone against to connect request from same browser.
+    num = 1
+    for k in users.keys():
+        if nickname in k:
+            num += 1
+    if num > 1:
+        nickname += " (" + str(num) + ")"
+    users[nickname] = ws
+
+    # Online users list
+    await r.zadd('name', 1, nickname)
     name_list = await r.zrange('name', 0, -1)
     print(name_list)
 
-    users[nickname] = ws
+    # Broadcast new user enter the room.
+    await broadcast(users, type="user", nickname=nickname, name_list=name_list)
 
-    for _ws in users.values():
-        await _ws.send_json({"type": "user", "nickname": nickname, "name_list": name_list})
-    
     while True:
+        # Receive message.
         msg = await ws.receive()
 
+        # Broadcast what received.
         if msg.type == aiohttp.WSMsgType.TEXT:
-            msg_count = int(await r.zcard('name'))
-            for _ws in users.values():
-                await _ws.send_json({"type": "received", "nickname": nickname, "message": msg.data})
-        elif msg.type == aiohttp.WSMsgType.CLOSE:
-            for _ws in users.values():
-                if ws != _ws:
-                    await _ws.send_json({"type": "disconnect", "nickname": nickname, "message": "Disconnected", "name_list": name_list})
+            # print(f'{nickname} sent message')
+            await broadcast(
+                users,
+                type="received",
+                nickname=nickname,
+                message=msg.data
+            )
+        # Remove user from list and close socket.
+        else:
+            print(f'{nickname} ws connection closed')
+            await r.zrem('name', nickname)
+            name_list = await r.zrange('name', 0, -1)
+            await broadcast(
+                users,
+                type="disconnect",
+                nickname=nickname,
+                name_list=name_list
+            )
+            # print(name_list)
+            del users[nickname]
+            await ws.close()
             break
-        elif msg.type == aiohttp.WSMsgType.ERROR:
-            for _ws in user_list.values():
-                if ws != _ws:
-                    await _ws.send_json({"type": "disconnect", "nickname": nickname, "message": "Disconnected", "name_list": name_list})
-            break
-
-    await r.zrem('name', nickname)
-    del users[nickname]
-    
-    
-
